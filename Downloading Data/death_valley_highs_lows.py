c76b31ae123e9b8e8afa8b73668cac5132117e0a
Heino <heinoduplessis@gmail.com>
2021-06-02 09:09:48 +0200

    Examining JSON data

--- Downloading Data/death_valley_highs_lows.py
+++ Downloading Data/death_valley_highs_lows.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/death_valley_2018_simple.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
@@ -11,11 +11,16 @@ with open(filename) as f:
 
 	for row in reader:
 		current_date = datetime.strptime(row[2], '%Y-%m-%d')
-		high = int(row[5])
-		low = int(row[6])
-		dates.append(current_date)
-		highs.append(high)
-		lows.append(low)
+		try:
+			high = int(row[4])
+			low = int(row[5])
+		except ValueError:
+			print(f"Missing data for {current_date}")
+		else:
+
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
 	#print(highs)
 
 # Plot the high and low temperatures.
@@ -26,10 +31,10 @@ ax.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high and low temperatures - 2018", fontsize=24)
+title = "Daily high and low temperatures - 2018\nDeath Valley, CA"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 plt.show()
-
